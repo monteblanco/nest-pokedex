58a7d7b8fd34f150271508b5f9b930e330a591aa
monteblanco <victor.guzman@monte-blanco.com>
2024-07-13 19:10:36 -0400

    Version ready for Cloud

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,21 +1,54 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
-  private readonly axios: AxiosInstance = axios;
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+
+    // Para no depender de un paquete de terceros se crea un adaptador que envuelva Axios
+    // private readonly axios: AxiosInstance = axios;
+    private readonly http: AxiosAdapter,
+  ) {}
 
   async executeSeed() {
-    const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=10',
+    await this.pokemonModel.deleteMany({}); // Limpia la tabla. delete * from pokemons
+
+    const data = await this.http.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
 
-    data.results.forEach(({ name, url }) => {
+    // Opción 1: Para optimizar inserción y no tener un await por cada inserción una opción es crear un arreglo de promesas y luego insertarlas todas.
+    // const insertPromisesArray = [];
+
+    // Opción 2:
+    const pokemonToInsert: { name: string; no: number }[] = [];
+
+    data.results.forEach(async ({ name, url }) => {
       const segments = url.split('/');
       const no: number = +segments[segments.length - 2];
+
+      // const pokemon = await this.pokemonModel.create({ name, no }); // Optimizar y no hacer un await por cada inserción.
+
+      // Opción 1: Poblar un arreglo con promesas
+      // insertPromisesArray.push(this.pokemonModel.create({ name, no }));
+
+      // Opción 2:
+      pokemonToInsert.push({ name, no });
     });
 
-    return data.results;
+    // Opción 1: Insertar arreglo de promesas
+    // await Promise.all(insertPromisesArray);
+
+    // Opción 2: Insertar arreglo en base de datos
+    await this.pokemonModel.insertMany(pokemonToInsert);
+
+    return 'Seed executed';
   }
 }
